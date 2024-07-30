d4c92d4b4d5ccc9b66764496623974caf88ac58c
selahattin <selahattinasn@gmail.com>
2024-07-31 00:32:20 +0200

    Updated

--- push.sh
+++ push.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-echo Hello, gib bitte die GitHub-Adresse ein?
-read ADRESS
-
-git clone $ADRESS
-cd ./${ADRESS:33:-4}
-echo Hello, soll ich die Code pushen?
-read ADRESS2
 git add .
-git commit -m "updated"
-git push https://<TOKE>@${ADRESS:8}
+git commit -m "Updated"
+git push origin main
