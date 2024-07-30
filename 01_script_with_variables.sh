db5c91ea574cec84a71bf3c750e6486c74f48fda
selahattin <selahattinasn@gmail.com>
2024-07-31 00:54:03 +0200

    Updated

--- 01_script_with_variables.sh
+++ 01_script_with_variables.sh
@@ -6,6 +6,8 @@
 #► Thus, you should consider those as 2 different types of variables
 
 name="Selim"
-content="$(ls -l ~)"
+content="$(ls -l ~)"     # a local variable here I could use then the command substitution to get the contents of home_directory
+# by tht we can bundle a command in a variable and use it in our script: 
+
 echo "Selam sana $name"
 echo "Your files are $content" | wc -l
